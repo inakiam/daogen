bebf8868e6821986efeaf3271049082d9bf39a78
Cyaed <6502x86@gmail.com>
2016-11-12 09:09:50 -0700

    cleanup + plurals added

--- assets/dao.js
+++ assets/dao.js
@@ -28,66 +28,82 @@ function seeds(input){
 		
 	}
 	
-	var a = name.hashCode();
-	var b = a.toString().hashCode();
-	var c = b.toString().hashCode();
-	var d = c.toString().hashCode();
 	
-	return [a,b,c,d];
+	
+	var a = [name.hashCode()];
+	
+	for (var i = 0; i < 10; i++){
+		a.push(a[a.length - 1].toString().hashCode());
+	}
+	
+	return a;
 	
 	
 }
 
-//This one can be touched in areas I marked as safe.
-function daoName(RWX,seed){
+function pluralize(term){
+	var plurals = [["Kni","fe","ves"]];
+	var unPterm = term;
+	var items;
 	
-	//Deathblade - add daoist titles, adjectives, and nouns to the inside of the square brackets
-	//on the appropraitely named vars. Use the format - words in quotes, separated by commas
-	//that I have used. Make sure each word is separated by a comma. 
+	for(var i = 0; i < plurals.length;i++){
+		items = plurals[i];
+		term = (items[0] + items[1] == unPterm) ? items[0] + items[2] : term;
+		
+	}
+		
+	return (term == unPterm) ? term + "s" : term;
+	
+}
+
+//This one can be touched in areas I marked as safe.
+function daoName(rwxmode,seed){
 	
 	//Safe-to-edit vars
 	var titles = ["Eccentric","Patriarch","Dao Lord","Immortal","Saint","Paragon"];
 	var adj = ["Slow","Fast","Quick","Silent","Subtle","True","False","Light"];
-	var nouns = ["Gold","Sun","World","Fire","Yin","Yang","Truth"];
+	var pluralisers = ["Thousand","Hundred"]; //adjectives that make nouns plural
+	var nouns = ["Knife","Kine"]//["Gold","Sun","World","Fire","Yin","Yang","Truth"];
+	
 	
 	var bp = "The Diviniation is complete.<br /><br /> You are... "
 	
+	var an;//chose which arrar is first part of name.
+	var indices = [];
 	
-	//Touching anything below this breaks the program.
-
+	var title;
+	
 	if(!seed){
 		//set adj or noun	
-		var an = Math.floor(Math.random()*2) == 0 ? adj:nouns;
+		an = [adj,nouns,pluralisers][Math.floor(Math.random()*3)];
 		
-		var zeroDex = Math.floor(Math.random() * titles.length);
-		var firstDex = Math.floor(Math.random()* an.length);
-		var secDex = Math.floor(Math.random() * nouns.length);
+		indices = [Math.floor(Math.random() * titles.length), Math.floor(Math.random()* an.length), Math.floor(Math.random() * nouns.length)];
 		
-		while (secDex == firstDex){
-			secDex = Math.floor(Math.random() * nouns.length);
-		}
 	}
 	
 	else{
 		
-		var an = [adj,nouns][Math.abs(seed[0]%2)];
+		an = [adj,nouns,pluralisers][Math.abs(seed[0]%3)];
 		
-		var zeroDex = Math.abs(seed[1] % (titles.length - 1));
-		var firstDex = Math.abs(seed[2] % (an.length - 1));
-		var secDex = Math.abs(seed[3] % (nouns.length - 1));
+		indices = [Math.abs(seed[1] % (titles.length - 1)), Math.abs(seed[2] % (an.length - 1)),  Math.abs(seed[3] % (nouns.length - 1))]
 		
-		if (secDex == firstDex){
-			
-			secDex = secDex+1 % (nouns.length - 1);
-			
-		}
 		
 	}
 	
-	if(RWX){
-		return bp + titles[zeroDex] + " " + toTitleCase(an[firstDex] + nouns[secDex]) + "!!!";
+	//prevent duplicate titles.
+	if (indices[1] == indices[2]){
+			
+		indices[2] = indices[2] + 1 % (nouns.length - 1);
+			
+	}
+	
+	title = [titles[indices[0]], an[indices[1]], (an == pluralisers) ? pluralize(nouns[indices[2]]) : nouns[indices[2]]]
+
+	
+	if(rwxmode){
+		return bp + title[0] + " " + toTitleCase(title[1] + title[2]) + "!!!";
 	}
 	else{
-		return bp + titles[zeroDex] + " " + toTitleCase(an[firstDex] + " " + nouns[secDex]) + "!!!";
+		return  bp + title[0] + " " + toTitleCase(title[1] + " " + title[2]) + "!!!";
 	}
 }
